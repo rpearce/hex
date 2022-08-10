9d8f4bb5429cdacd69737e0c84c5b3380fb77919
Robert W. Pearce <me@robertwpearce.com>
2022-08-10 07:39:08 -0400

    Fix: rgbaToHex (#23)

--- source/rgbaToHex.ts
+++ source/rgbaToHex.ts
@@ -5,12 +5,18 @@ export interface RgbaToHex {
 }
 
 const rgbaToHex: RgbaToHex = ([r = 0, g = 0, b = 0, a = 1] = []) => {
-  const rgbN = (1 << 24) | (r << 16) | (g << 8) | b
-  const hex = rgbN.toString(16).slice(1)
+  const hex = [r, g, b].map(x =>
+    Math
+      .min(255, Math.max(0, Math.round(x)))
+      .toString(16)
+      .padStart(2, '0')
+  ).join('')
+
   const alpha = (a * 255)
     .toString(16)
     .padStart(2, '0')
     .slice(0, 2)
+
   return `#${hex}${alpha}`
 }
 
